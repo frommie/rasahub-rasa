555bc7b18ea786a3acc1a2e578dd17919252ff17
frommie <christian.frommert@gmail.com>
2018-05-25 11:56:16 +0200

    Same version for all main components of Rasahub

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extras_requires = {
 }
 
 setup(name='rasahub-rasa',
-      version='0.0.1',
+      version='0.3.0',
       description='Rasa connector for Rasahub',
       url='http://github.com/frommie/rasahub-rasa',
       author='Christian Frommert',
